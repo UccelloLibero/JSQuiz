1ae2aa0588d5ddcfb024ade9a6933c1075c8d818
Maya <maya.husic@gmail.com>
2018-10-09 08:25:47 -0400

    first commit

---
+++ quiz.js
@@ -0,0 +1,6 @@
+var answer = prompt('What is the highest mountain in the state of Colorado?');
+if ( answer.toUpperCase() === 'MT ELBERT' ) {
+  document.write("<p>That's right!</p>");
+} else {
+  document.write("<p>That's incorrect.</p>");
+}
